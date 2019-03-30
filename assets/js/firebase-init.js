c554e0f2862f364fd51e3a54748856981d089768
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-03-29 18:26:58 -0700

    Added and integrated empty asset files and scripts into project

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,11 @@
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAfbmcv-VJ4PuXQ3actT59u2-aU7iIEV0s",
+    authDomain: "date-night-for-dummies.firebaseapp.com",
+    databaseURL: "https://date-night-for-dummies.firebaseio.com",
+    projectId: "date-night-for-dummies",
+    storageBucket: "date-night-for-dummies.appspot.com",
+    messagingSenderId: "191871767511"
+});
+
+database = firebase.database();
