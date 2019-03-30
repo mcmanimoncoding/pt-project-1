24ec4ad8e79815fe9291ec82e8390016b1a55cdd
Daniel M <mcmanimoncoding@gmail.com>
2019-03-30 13:21:11 -0700

    updated files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 
 // var datepicker = UIkit.datepicker(element, { /* options */ });
-$('.datepicker').datepicker();
-$('.datepicker').datepicker({
-    format: 'mm/dd/yyyy',
-    startDate: '-3d'
-});
+// $('.datepicker').datepicker();
+// $('.datepicker').datepicker({
+//     format: 'mm/dd/yyyy',
+//     startDate: '-3d'
+// });
 
 
 var newLocation;
