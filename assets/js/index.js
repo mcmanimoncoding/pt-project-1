60b9d59a7171f3f45923b2dd86dddfdd4c2e6096
Lexi127 <aperalta127@gmail.com>
2019-03-30 12:54:37 -0700

    rud

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,6 @@
-var datepicker = UIkit.datepicker(element, { /* options */ });
+// var datepicker = UIkit.datepicker(element, { /* options */ });
+$('.datepicker').datepicker();
+$('.datepicker').datepicker({
+    format: 'mm/dd/yyyy',
+    startDate: '-3d'
+});
