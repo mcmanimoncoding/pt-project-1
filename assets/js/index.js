33e9616f58af2cd1f2ede01da757eae05c42f992
Daniel M <mcmanimoncoding@gmail.com>
2019-03-30 13:36:42 -0700

    defer JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,12 @@
 
-// var datepicker = UIkit.datepicker(element, { /* options */ });
-// $('.datepicker').datepicker();
-// $('.datepicker').datepicker({
-//     format: 'mm/dd/yyyy',
-//     startDate: '-3d'
-// });
-
 
 var newLocation;
 var newDay;
 
+console.log("active")
+
 $("#search-button").on("click", function () {
-    event.preventDefault()
+    event.preventDefault();
     newLocation = $("#location-input").val().trim();
     newDay = $("#day-input").val();
     console.log(newLocation + " : " + newDay);
