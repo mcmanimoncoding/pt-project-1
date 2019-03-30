ba015a3d447a57c6a7797a4003dd4594f358d2b2
Daniel M <mcmanimoncoding@gmail.com>
2019-03-30 14:06:26 -0700

    Autocomplete Search

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,12 @@ $("#search-button").on("click", function () {
     newLocation = $("#location-input").val().trim();
     newDay = $("#day-input").val();
     console.log(newLocation + " : " + newDay);
+    console.log(placesAutocomplete);
 });
 
 
+  var placesAutocomplete = places({
+    appId: 'plUKJGJ6LEVW',
+    apiKey: 'b28ccea8a5ebad3289976644f86bd0c4',
+    container: document.querySelector('#location-input')
+  });
