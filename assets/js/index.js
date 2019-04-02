604485e4304df112b31570e3597c136375c51006
Daniel M <mcmanimoncoding@gmail.com>
2019-04-01 21:08:17 -0700

    Location and Datepicker

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,27 @@
-// var datepicker = UIkit.datepicker(element, { /* options */ });
-// $('.datepicker').datepicker();
-// $('.datepicker').datepicker({
-//     format: 'mm/dd/yyyy',
-//     startDate: '-3d'
-// });
-
 
 var newLocation;
 var newDay;
+var placesAutocomplete = places({
+  appId: 'plUKJGJ6LEVW',
+  apiKey: 'b28ccea8a5ebad3289976644f86bd0c4',
+  container: document.querySelector('#location-input')
+});
 
 $("#search-button").on("click", function () {
     event.preventDefault()
     newLocation = $("#location-input").val().trim();
-    newDay = $("#day-input").val();
+    newDay = $("#datepicker-3").val();
     console.log(newLocation + " : " + newDay);
 });
+
+$(function() {
+    $( "#datepicker-3" ).datepicker({
+       appendText:"(dd-mm-yy)",
+       dateFormat:"dd-mm-yy",
+       altField: "#datepicker-4",
+       altFormat: "DD, d MM, yy"
+    });
+ });
+
 
 
