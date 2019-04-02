94e761961ece3126c962cbd281e7269400705522
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-04-01 20:11:25 -0700

    Added local storage saving of user inputs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,31 @@
 
-var newLocation;
-var newDay;
-
-console.log("active")
-
-$("#search-button").on("click", function () {
-  event.preventDefault();
-  newLocation = $("#location-input").val().trim();
-  newDay = $("#day-input").val();
-  console.log(newLocation + " : " + newDay);
-  console.log(placesAutocomplete);
-});
-
-
 var placesAutocomplete = places({
   appId: 'plUKJGJ6LEVW',
   apiKey: 'b28ccea8a5ebad3289976644f86bd0c4',
   container: document.querySelector('#location-input')
+});
+
+let lat, lng;
+
+placesAutocomplete.on("change", event => {
+  lat = event.suggestion.latlng.lat;
+  lng = event.suggestion.latlng.lng;
+  console.log(lat, lng);
+});
+
+placesAutocomplete.on("clear", event => {
+  lat = undefined; lng = undefined;
+})
+
+$("#search-button").on("click", function () {
+  event.preventDefault();
+  let datePicked = $("#day-input").val();
+  let locationName = $("#location-input").val();
+  if (lat && lng) {
+    let transferData = {locationName, datePicked, lat, lng};
+    window.localStorage.setItem("indexData", JSON.stringify(transferData));
+    window.open("./find-events.html", "_self");
+  } else {
+    // TODO: Display error for incorrectly chosen location.
+  }
 });
