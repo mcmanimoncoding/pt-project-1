6f8c4ee68b2df769de82728fe1449654d42a49e4
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-04-01 18:59:05 -0700

    Fixed other file formatting

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,11 @@ var newDay;
 console.log("active")
 
 $("#search-button").on("click", function () {
-    event.preventDefault();
-    newLocation = $("#location-input").val().trim();
-    newDay = $("#day-input").val();
-    console.log(newLocation + " : " + newDay);
-    console.log(placesAutocomplete);
+  event.preventDefault();
+  newLocation = $("#location-input").val().trim();
+  newDay = $("#day-input").val();
+  console.log(newLocation + " : " + newDay);
+  console.log(placesAutocomplete);
 });
 
 
