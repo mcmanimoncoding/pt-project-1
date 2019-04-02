733db923ccb6debc3481221a58c2ef70305c798d
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-04-01 21:40:34 -0700

    Adding code for find-events and index

--- assets/js/find-events.js
+++ assets/js/find-events.js
@@ -1,13 +1,21 @@
 
 // TODO: Get user-inputted locally-stored location info
 
-// Using postal code as a placeholder for now
-Ticketmaster.search({ postalCode: "85224" })
-    .then(data => {
-        let { _embedded: { events } } = data;
-        events.forEach(rawEventData => {
-            // TODO: Add code for displaying events to the end-user and handling event selection here
+$(_ => {
+    // Get loaded data from local storage
+    let data = window.localStorage.getItem("indexData");
+    let loadedData = JSON.parse(data);
+
+    // Using postal code as a placeholder for now
+    Ticketmaster.search({ postalCode: "85224" })
+        .then(data => {
+            let { _embedded: { events } } = data;
+            events.forEach(rawEventData => {
+                // TODO: Add code for displaying events to the end-user and handling event selection here
+            });
         });
-    });
+})
+
+
 
 // TODO: Add "loading" animation to page before results are displayed
