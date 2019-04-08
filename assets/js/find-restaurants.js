1b24be5243b943f67ae7221cbb42fedebdaac687
Daniel M <mcmanimoncoding@gmail.com>
2019-04-07 22:41:55 -0700

    restaurant Storage

--- assets/js/find-restaurants.js
+++ assets/js/find-restaurants.js
@@ -1,4 +1,3 @@
-
 // Get loaded data from local storage
 let data = window.localStorage.getItem("indexData");
 if (!data) window.open("./", "_self");
@@ -6,10 +5,16 @@ let loadedData = JSON.parse(data);
 
 $(_ => {
     console.log("Zomato");
-    Zomato.search({ lon: loadedData.lng, lat: loadedData.lat })
+    Zomato.search({
+            lon: loadedData.lng,
+            lat: loadedData.lat
+        })
         .then(response => {
             console.log(response);
-            let { restaurants, results_found: resultCount } = response;
+            let {
+                restaurants,
+                results_found: resultCount
+            } = response;
             console.log(response);
             restaurants.forEach((rawRestaurantData, index) => {
                 console.log(rawRestaurantData);
@@ -18,16 +23,30 @@ $(_ => {
 
                 console.log(rawRestaurantData.restaurant.photos_url);
 
-                $(res +" .uk-card-media").attr("src", rawRestaurantData.restaurant.featured_image);
+                $(res + " .uk-card-media").attr("src", rawRestaurantData.restaurant.featured_image);
 
-                $(res+" #rest-title").text(rawRestaurantData.restaurant.name);
+                $(res + " #rest-title").text(rawRestaurantData.restaurant.name);
 
-                $(res+" .rest-address").text(rawRestaurantData.restaurant.location.address);
+                $(res + " .rest-address").text(rawRestaurantData.restaurant.location.address);
 
-                $(res+" #rest-details").text("Rating: "+rawRestaurantData.restaurant.user_rating.aggregate_rating+" out of 5").append("<br>Cuisine: "+rawRestaurantData.restaurant.cuisines).append("<br><br><a href='"+rawRestaurantData.restaurant.menu_url+"'><h3>Check Out The Menu!</h3></a>");
-              
-              
-              
+                $(res + " #rest-details").text("Rating: " + rawRestaurantData.restaurant.user_rating.aggregate_rating + " out of 5").append("<br>Cuisine: " + rawRestaurantData.restaurant.cuisines).append("<br><br><a href='" + rawRestaurantData.restaurant.menu_url + "'><h3>Check Out The Menu!</h3></a>");
+
+
+
+                $(res + " button").on("click", function (event) {
+                    console.log("click")
+                    event.preventDefault();
+                    var savedRestData = {
+                        restaurant: {
+                            restName: rawRestaurantData.restaurant.name,
+                            restAddress: rawRestaurantData.restaurant.location.address,
+                            restImg: rawRestaurantData.restaurant.featured_image,
+                            restCuisine: rawRestaurantData.restaurant.cuisines,
+                        }
+                    }
+                    saveRestData(savedRestData);
+                    window.open("./itinerary.html", "_self");
+                });
             });
         })
         .catch(err => {
@@ -42,30 +61,18 @@ $(_ => {
             $("#find-restaurants-loading-display").remove();
         });
 
-        $(res + " button").on("click", function (event) {
-            event.preventDefault();
-            var savedRestData = {
-                restaurant: {
-                    restName: rawRestaurantData.restaurant.name,
-                    restAddress: rawEventData.restaurant.location.address,
-                    restImg: rawEventData.restaurant.featured_image,
-                    restCuisine: rawEventData._embedded.venues["0"].location,
-                }
-            }
-            saveEventData(savedEventData);
-            window.open("./find-restaurants.html", "_self");
-        });
-    
+
+
     /**
      * Caches the the given object into local storage for later use.
      * Local storage entry is "savedRestaurantData".
      * 
      * *NOTE*: Object is saved to local storage as JSON, do not forget to call `JSON.parse()`
      * 
-     * @param  {Object} savedEventData Object that will be saved to local storage
+     * @param  {Object} savedRestData Object that will be saved to local storage
      */
-    function saveRestaurantData(savedRestaurantData) {
-        window.localStorage.setItem("savedRestaurantData", JSON.stringify(savedRestaurantData));
+    function saveRestData(savedRestData) {
+        window.localStorage.setItem("savedRestaurantData", JSON.stringify(savedRestData));
     }
 
     // TODO: Add "loading" animation to page before results are displayed
